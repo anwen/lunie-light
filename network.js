1a1fe59bc38dd2dba7d4d12f7b6e1cb8c676ba0c
askender <askender43@gmail.com>
2021-04-05 22:27:21 +0800

    Update network.js

--- network.js
+++ network.js
@@ -5,7 +5,7 @@ export default {
     'LikeCoin is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
   logo: `logo.svg`,
   website: 'https://like.co',
-  apiURL: 'http://localhost:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  apiURL: 'http://116.207.131.37:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
   rpcURL: 'ws://116.207.131.37:26657',
   stakingDenom: 'MUON',
   coinLookup: [
